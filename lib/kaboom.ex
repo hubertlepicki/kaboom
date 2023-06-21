396ffa530ceb14851ca28176494b7fa21cfe6938
Hubert Łępicki <hubert.lepicki@amberbit.com>
2023-06-21 15:10:29 +0200

    Initial commit, shows a failure

---
+++ lib/kaboom.ex
@@ -0,0 +1,18 @@
+defmodule Kaboom do
+  @moduledoc """
+  Documentation for `Kaboom`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kaboom.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kaboom_test.exs
@@ -0,0 +1,39 @@
+defmodule KaboomTest do
+  use ExUnit.Case, async: true
+
+  import ExUnit.CaptureLog
+
+  require Logger
+
+  test "log some debugs" do
+    assert capture_log(fn ->
+             Enum.each((0..1000), fn i ->
+               Logger.debug("log msg #{i}")
+             end)
+           end) =~ "log msg"
+  end
+
+  test "log some warnings" do
+    assert capture_log(fn ->
+             Enum.each((0..1000), fn i ->
+               Logger.warning("log msg #{i}")
+             end)
+           end) =~ "log msg"
+  end
+
+  test "log some infos" do
+    assert capture_log(fn ->
+             Enum.each((0..1000), fn i ->
+               Logger.info("log msg #{i}")
+             end)
+           end) =~ "log msg"
+  end
+
+  test "log some errors" do
+    assert capture_log(fn ->
+             Enum.each((0..1000), fn i ->
+               Logger.error("log msg #{i}")
+             end)
+           end) =~ "log msg"
+  end
+end
